c502b5e6e26708c3df3486eff8aa389c248d77c3
sreejith87 <sreejith.darmatek@gmail.com>
2018-08-07 17:56:33 +0800

    eighth commit

--- www/geolocation.js
+++ www/geolocation.js
@@ -89,10 +89,9 @@ var geolocation = {
             //clearTimeout(timeoutTimer.timer);
             //timeoutTimer.timer = null;
             //var err = new PositionError(e.code, e.message);
-            //if (errorCallback) {
-                //errorCallback(err);
-            //}
-            alert('error' + e);
+            if (errorCallback) {
+                errorCallback(err);
+            }
         };
 
         // exec(win, fail, 'Geolocation', 'getLocation', [options.enableHighAccuracy, options.maximumAge]);
